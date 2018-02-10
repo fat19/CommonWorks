2e83b8891a47ca2de9342d22d93305e511183f6a
fat19 <altin.fth19@gmail.com>
2018-02-10 07:38:44 +0300

    Define Variable
    Defined a test variable

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const deneme = 'Fatih';
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
